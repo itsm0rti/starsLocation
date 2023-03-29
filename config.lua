ec1ae1716734ee5f59218393199f8fe8e62cebbd
morti <122681615+morti-dev@users.noreply.github.com>
2023-03-29 16:02:16 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+Config.LogsWebhook = ""
+
+Config.MarkerDistance = 15 -- Distance minimum pour pouvoir voir le marker
+Config.MarkerType = 2 -- Type du marker (https://docs.fivem.net/docs/game-references/markers/)
+Config.MarkerColor = {r = 244, g = 5, b = 80, a = 200} -- Couleur du marker (rgba)
+
+Config.BlipName = "Location" -- Nom du blip
+Config.BlipSprite = 226 -- Id du blip (https://docs.fivem.net/docs/game-references/blips/)
+Config.BlipColor = 0 -- Couleur du blip (https://docs.fivem.net/docs/game-references/hud-colors/)
+
+Config.Blur = true -- Activer le flou en arrière plan lorsque le menu est ouvert
+
+-- Coordonées des locations
+Config.Locations = {
+    vector3(188.03, -842.01, 30.99)
+}
+
+
+-- Pour configurer les véhicules, rendez-vous dans ui/index.js
+
